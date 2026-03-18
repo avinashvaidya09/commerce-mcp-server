ef284fe840a220d7f55352d0aa70c7f2040198a0
Avinash Vaidya <avinashdevspace@gmail.com>
2026-03-18 10:26:07 -0400

    Integrated with Commerce API - get products

--- src/destination_client.py
+++ src/destination_client.py
@@ -73,8 +73,8 @@ class DestinationServiceClient:
                 if isinstance(token_type, str) and isinstance(token_value, str) and token_value:
                     auth_header = f"{token_type} {token_value}" if token_type else token_value
 
-        username = dest_cfg.get("User") if isinstance(dest_cfg.get("ClientId"), str) else None
-        password = dest_cfg.get("Password") if isinstance(dest_cfg.get("ClientSecret"), str) else None
+        username = dest_cfg.get("clientId")
+        password = dest_cfg.get("clientSecret")
 
         return DestinationDetails(
             name=destination_name,
